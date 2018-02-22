a2196a303b2cef423eb5a58d570c814ff43b06db
ANTHONY <ANTHONY@LAPTOP-UQEQU1JS>
2018-02-22 11:54:19 -0500

    entidad cliente

---
+++ Grupo5Banco/src/grupo5banco/rnegocio/dao/ICliente.java
@@ -0,0 +1,14 @@
+
+package grupo5banco.rnegocio.dao;
+
+import grupo5banco.rnegocio.entidades.Cliente;
+import java.util.List;
+
+
+public interface ICliente {
+    int insertar(Cliente cliente) throws Exception;
+    int modificar(Cliente cliente) throws Exception;
+    int eliminar(Cliente cliente) throws Exception;
+    Cliente obtener(int codEpl) throws Exception;
+    List<Cliente> obtener() throws Exception;
+}
