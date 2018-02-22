9ef7f2591ec5d439cd157621f272f55881a35d2e
stian <stian@DESKTOP-T7F4CVQ>
2018-02-22 09:01:24 -0500

    cuentas

---
+++ Grupo5Banco/src/grupo5banco/rnegocio/dao/ICu_Ahorro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo5banco.rnegocio.dao;
+
+import grupo5banco.rnegocio.entidades.*;
+import java.util.*;
+
+/**
+ *
+ * @author stian
+ */
+public interface ICu_Ahorro {
+      public int insertar(Cueahorros cueahorro) throws Exception;
+    public int modificar(Cueahorros cueahorro) throws Exception;
+    public int eliminar(Cueahorros cueahorro) throws Exception;
+    public Pedido obtener(int codigo) throws Exception;
+    public List<Cueahorros> obtener() throws Exception;  
+}
