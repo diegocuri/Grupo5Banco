515eef3325ee5a9d5f5ab17a64e43cee142c3c67
Danny <User@PC>
2018-02-26 15:57:35 -0500

    correcion

--- Grupo5Banco/src/grupo5banco/rnegocio/dao/ISucursal.java
+++ Grupo5Banco/src/grupo5banco/rnegocio/dao/ISucursal.java
@@ -1,17 +1,17 @@
-
 package grupo5banco.rnegocio.dao;
 
 import grupo5banco.rnegocio.entidades.Sucursal;
 import java.util.List;
 
 public interface ISucursal {
-     int insertar(Sucursal sucursal) throws Exception;
-    
-    int modificar(Sucursal sucursal) throws Exception;
-    
-    int eliminar(Sucursal sucursal) throws Exception;
-    
-    Sucursal obtener(int codSu) throws Exception;
-    
-    List<Sucursal> obtener() throws Exception;
+
+    public int insertar(Sucursal sucursal) throws Exception;
+
+    public int modificar(Sucursal sucursal) throws Exception;
+
+    public int eliminar(Sucursal sucursal) throws Exception;
+
+    public Sucursal obtener(int codSu) throws Exception;
+
+    public List<Sucursal> obtener() throws Exception;
 }
