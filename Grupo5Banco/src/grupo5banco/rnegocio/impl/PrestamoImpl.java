ed8454f8c6c1442fbaa6323609360292209019bc
FLAVIO <>
2018-02-27 12:08:22 -0500

    prestamo impl...vista

--- Grupo5Banco/src/grupo5banco/rnegocio/impl/PrestamoImpl.java
+++ Grupo5Banco/src/grupo5banco/rnegocio/impl/PrestamoImpl.java
@@ -5,39 +5,140 @@
  */
 package grupo5banco.rnegocio.impl;
 
-import grupo5banco.accesodatos.*;
+
+import grupo5banco.accesodatos.Conexion;
+import grupo5banco.accesodatos.Parametro;
 import grupo5banco.rnegocio.dao.*;
 import grupo5banco.rnegocio.entidades.*;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrestamoImpl implements IPrestamo{
+/**
+ *
+ * @author ANTHONY
+ */
+public class PrestamoImpl implements IPrestamo {
 
     @Override
     public int insertar(Prestamo prestamo) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         int numFilasAfectadas = 0;
+        String sql = "INSERT INTO prestamo(\n"
+                + "            codPre_prestamo, codSu_sucrusal, )\n"
+                + "    VALUES (?, ?,);";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, prestamo.getCodPre()));
+        lstPar.add(new Parametro(2, prestamo.getSucursal().getCodSu()));
+        
+
+        Conexion con = new Conexion();
+        con.conectar();
+        try {
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
+      
     }
 
     @Override
     public int modificar(Prestamo prestamo) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          int numFilasAfectadas = 0;
+        String sql = "UPDATE prestamo\n"
+                + "   SET codPre_prestamo=? ,CodSu=?, \n"
+                + " WHERE codPre_prestamo=?";
+        Conexion con = new Conexion();
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, prestamo.getCodPre()));
+        lstPar.add(new Parametro(2, prestamo.getSucursal().getCodSu()));
+       
+
+        con.conectar();
+        try {
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
     }
 
     @Override
     public int eliminar(Prestamo prestamo) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          int numFilasAfectadas = 0;
+        String sql = "DELETE FROM   Prestamo\n"
+                + " WHERE codPre=?;";
+        Conexion con = new Conexion();
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, prestamo.getCodPre()));
+        con.conectar();
+        try {
+            numFilasAfectadas = con.ejecutaComando(sql,lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
     }
 
     @Override
-    public Prestamo obtener(int codigo) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Prestamo obtener(int CodPre) throws Exception {
+        Prestamo prestamo = null;
+        String sql = "SELECT codPre ,codSu, \n"
+                + "  FROM prestamo where codPre=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, CodPre));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutarQuery(sql, lstPar);
+            while (rst.next()) {
+                prestamo = new Prestamo();
+                prestamo.setCodPre(rst.getInt(1));
+                ISucursal sucursaldao = new SucursalImpl();
+                Sucursal sucursal = sucursaldao.obtener(rst.getInt(2));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return prestamo;
     }
 
     @Override
     public List<Prestamo> obtener() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Prestamo> lista = new ArrayList<>();
+        String sql = "SELECT codPre ,codSu \n"
+                + "  FROM prestamo";
+        Conexion con = new Conexion();
+        con.conectar();
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutarQuery(sql, null);
+            Prestamo prestamo=null;
+            while (rst.next()) {
+                prestamo = new Prestamo();
+                prestamo.setCodPre(rst.getInt(1));
+                ISucursal sucursaldao = new SucursalImpl();
+                Sucursal sucursal = sucursaldao.obtener(rst.getInt(2));
+                lista.add(prestamo);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return lista;
     }
-    
-    
 }
+
+    
+    
---
+++ Grupo5Banco/src/grupo5banco/rnegocio/vistas/FrmNuevoPrestamo.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo5banco.rnegocio.vistas;
+
+import grupo5banco.rnegocio.dao.IPrestamo;
+import grupo5banco.rnegocio.dao.ISucursal;
+import grupo5banco.rnegocio.entidades.*;
+import grupo5banco.rnegocio.impl.PrestamoImpl;
+import grupo5banco.rnegocio.impl.SucursalImpl;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author ANTHONY
+ */
+public class FrmNuevoPrestamo extends JInternalFrame  {
+    List<Sucursal> lstSucursal;
+    JComboBox<Sucursal> cmbSucursal;
+
+    JLabel lblCodPre;
+    JLabel lblCodSu;
+    JLabel lblTitulo;
+    
+    JTextField txtCodPre;
+
+ 
+    JButton btnLimpiar;
+    JButton btnAceptar;
+    
+    JPanel pnlCentral;
+    JPanel pnlPie;
+
+    public FrmNuevoPrestamo() {
+        this.setSize(300, 300);
+        this.setLayout(new BorderLayout());
+        this.setClosable(true);
+        pnlCentral= new JPanel();
+        pnlPie= new JPanel();
+        pnlCentral.setLayout(new GridLayout(10, 2, 5, 5));
+        pnlPie.setLayout(new GridLayout(1,2,5,5));
+
+        lblTitulo = new JLabel("DATOS DE PEDIDO");
+        lblCodPre = new JLabel("codigo prestamo: ");
+        txtCodPre = new JTextField(1);
+        lblCodSu = new JLabel("PRESTAMO:");
+        cargarSucursal();
+        cmbSucursal= new JComboBox(lstSucursal.toArray());
+     
+  
+        pnlCentral.add(lblCodPre);
+        pnlCentral.add(txtCodPre);
+        pnlCentral.add(lblCodSu);
+        pnlCentral.add(cmbSucursal);
+        
+        btnLimpiar = new JButton("Limpiar");
+        btnAceptar = new JButton("Aceptar");
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    btnAceptarActionListener(e);
+                } catch (Exception ex) {
+                    System.out.println("Error:"+ex.getMessage());
+                }
+            }
+        });        
+        pnlPie.add(btnLimpiar);
+        pnlPie.add(btnAceptar);        
+        this.add(lblTitulo, BorderLayout.NORTH);
+        this.add(pnlCentral, BorderLayout.CENTER);
+        this.add(pnlPie, BorderLayout.SOUTH);        
+    }
+
+    public static void main(String[] args) {
+       FrmNuevoPrestamo frmMenu = new   FrmNuevoPrestamo();
+        frmMenu.setVisible(true);
+    }
+
+    public void cargarSucursal() {
+        ISucursal sucursalDao = new SucursalImpl();
+        try {
+            lstSucursal = sucursalDao.obtener();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Error al cargar sucursals!!",
+                    "Error" + e.getMessage(), JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+    
+   
+    public void btnAceptarActionListener(ActionEvent e) {
+        IPrestamo prestamoDao = new PrestamoImpl();
+        Prestamo prestamo = new Prestamo();
+        prestamo.setCodPre(Integer.parseInt(txtCodPre.getText()));
+        prestamo.setSucursal((Sucursal) cmbSucursal.getSelectedItem());
+     
+        try {
+            if(prestamoDao.insertar(prestamo)>0){
+                JOptionPane.showMessageDialog(this,"Guaradado correctamente!!",
+                "Transacción", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(this,"Error desconocido!!",
+                "Transacción", JOptionPane.INFORMATION_MESSAGE);
+            }           
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this,"Error al guardar!!: " + ex.getMessage(),
+                "Error", JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+      
+    }
+}
