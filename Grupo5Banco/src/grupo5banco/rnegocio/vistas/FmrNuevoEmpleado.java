3de875758e7f54cac36eb885bb7d88ac4f75033f
Danny <User@PC>
2018-02-27 06:15:07 -0500

    correcionn

--- Grupo5Banco/src/grupo5banco/rnegocio/vistas/FmrNuevoEmpleado.java
+++ Grupo5Banco/src/grupo5banco/rnegocio/vistas/FmrNuevoEmpleado.java
@@ -7,12 +7,13 @@ package grupo5banco.rnegocio.vistas;
 
 import grupo5banco.rnegocio.entidades.*;
 import grupo5banco.rnegocio.dao.*;
-import grupo5banco.rnegocio.impl.*; 
+import grupo5banco.rnegocio.impl.*;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -21,19 +22,15 @@ import javax.swing.JTextField;
 import javax.swing.*;
 import java.util.List;
 
-
 public class FmrNuevoEmpleado extends JInternalFrame {
 
     List<Sucursal> lstSucursal;
     JComboBox<Sucursal> cmbSucursal;
-
-    JPanel pnlCentral;
-    JPanel pnlPie;
     JLabel lblTitulo;
     JTextField txtTitulo;
-
     JLabel lblCodEpl;
     JTextField txtCodEpl;
+    JLabel lblSucursal;
     JLabel lblNombre;
     JTextField txtNombre;
     JLabel lblApellido;
@@ -43,18 +40,17 @@ public class FmrNuevoEmpleado extends JInternalFrame {
     JLabel lblantiguedad;
     JTextField txtantiguedad;
 
-    JLabel lblSucursal;
-    JLabel lblCiudad;
-    JComboBox cmdCiudad;
     JButton btnLimpiar;
     JButton btnAceptar;
+    JPanel pnlCentral;
+    JPanel pnlPie;
 
     public FmrNuevoEmpleado() {
-        this.setSize(400, 600);
+        this.setSize(300, 300);
         this.setLayout(new BorderLayout());
+        this.setClosable(true);
         pnlCentral = new JPanel();
         pnlPie = new JPanel();
-
         pnlCentral.setLayout(new GridLayout(10, 2, 5, 5));
         pnlPie.setLayout(new GridLayout(1, 2, 5, 5));
 
@@ -71,11 +67,23 @@ public class FmrNuevoEmpleado extends JInternalFrame {
         lblantiguedad = new JLabel("ANTIGUEDAD: ");
         txtantiguedad = new JTextField(2);
 
-  
+        txtCodEpl = new JTextField(2);
+        txtCodEpl = new JTextField(2);
+        txtNombre = new JTextField(2);
+        txtApellido = new JTextField(2);
+        txtfecha_vinculacion = new JTextField(2);
+        txtfecha_vinculacion = new JTextField(2);
+        txtantiguedad = new JTextField(2);
+        // cmbSexo = new JComboBox(new String[]{"masculino", "femenino"});
+        cargarSucursal();
+        cmbSucursal = new JComboBox(lstSucursal.toArray());
+        btnLimpiar = new JButton("Limpiar");
+        btnAceptar = new JButton("Aceptar");
 
         pnlCentral.add(lblCodEpl);
         pnlCentral.add(txtCodEpl);
-     
+        pnlCentral.add(lblSucursal);
+        pnlCentral.add(cmbSucursal);
         pnlCentral.add(lblNombre);
         pnlCentral.add(txtNombre);
         pnlCentral.add(lblApellido);
@@ -84,14 +92,7 @@ public class FmrNuevoEmpleado extends JInternalFrame {
         pnlCentral.add(txtfecha_vinculacion);
         pnlCentral.add(lblantiguedad);
         pnlCentral.add(txtantiguedad);
-     
-        lblSucursal = new JLabel("SUCURSAL");
-        cmbSucursal = new JComboBox(new String[]{" Ciudad", "Quito", "Cuenca", "Santo Domingo", "Machala", "Eloy Alfaro Duran", "Manta", "Portoviejo", "Riobamba", "Ambato"});
-        pnlCentral.add(lblSucursal);
-        pnlCentral.add(cmbSucursal);
-     
-        btnLimpiar = new JButton("Limpiar");
-        btnAceptar = new JButton("Aceptar");
+
         btnAceptar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -102,17 +103,17 @@ public class FmrNuevoEmpleado extends JInternalFrame {
                 }
             }
         });
+
         pnlPie.add(btnLimpiar);
         pnlPie.add(btnAceptar);
 
         this.add(lblTitulo, BorderLayout.NORTH);
         this.add(pnlCentral, BorderLayout.CENTER);
         this.add(pnlPie, BorderLayout.SOUTH);
-
     }
 
     public static void main(String[] args) {
-       FmrNuevoEmpleado frmMenu = new   FmrNuevoEmpleado();
+        FmrNuevoEmpleado frmMenu = new FmrNuevoEmpleado();
         frmMenu.setVisible(true);
     }
 
@@ -121,13 +122,42 @@ public class FmrNuevoEmpleado extends JInternalFrame {
         try {
             lstSucursal = sucursalDao.obtener();
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Error al cargar las ciudades!!",
+            JOptionPane.showMessageDialog(this, "Error al cargar las sucursales!!",
                     "Error" + e.getMessage(), JOptionPane.ERROR_MESSAGE);
         }
+
     }
-    
+
     public void btnAceptarActionListener(ActionEvent e) {
-        JOptionPane.showMessageDialog(this, "proceso correcto!!",
-                "Transacción", JOptionPane.INFORMATION_MESSAGE);
+        IEmpleado empleadoDao = new EmpleadoImpl();
+        Empleado empleado = new Empleado();
+        empleado.setCodEpl(Integer.parseInt(txtCodEpl.getText()));
+        empleado.setSucursal((Sucursal) cmbSucursal.getSelectedItem());
+        empleado.setNombre(txtNombre.getText());
+        empleado.setApellido(txtApellido.getText());
+
+        DateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            empleado.setFecha_vinculacion(formatoFecha.parse(txtfecha_vinculacion.getText()));
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error en la fecha!!",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        empleado.setAntiguedad(Integer.parseInt(txtantiguedad.getText()));
+
+        try {
+            if (empleadoDao.insertar(empleado) > 0) {
+                JOptionPane.showMessageDialog(this, "Guardado Correctamente!!",
+                        "Transacción", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(this, "Error desconocido: ",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error al guardar!!: " + ex.getMessage(),
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
 }
